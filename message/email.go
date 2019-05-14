d296ec626ff336fc828418cf2e0c1ca3a365441d
charlesbases <1162776833@qq.com>
2019-05-14 15:01:44 +0800

    email

--- message/email.go
+++ message/email.go
@@ -8,24 +8,15 @@ import (
 
 // EmailMessage 内容
 type EmailMessage struct {
-	From        string
-	To          []string
-	Cc          []string
-	Subject     string
-	ContentType string
-	Content     string
-	Attach      string
+	From        string   // 发件人
+	To          []string // 收件人
+	Cc          []string // 抄送人
+	Subject     string   // 标题
+	ContentType string   // 内容类型 text/plain text/html
+	Content     string   // 内容
+	Attach      string   // 附件
 }
 
-/*
- NewEmailMessage    返回消息对象
- from               发件人
- subject            标题
- contentType        内容的类型 text/plain text/html
- attach             附件
- to                 收件人
- cc                 抄送人
-*/
 func NewEmailMessage(from, subject, contentType, content, attach string, to, cc []string) *EmailMessage {
 	return &EmailMessage{
 		From:        from,
@@ -40,18 +31,13 @@ func NewEmailMessage(from, subject, contentType, content, attach string, to, cc 
 
 // EmailClient 发送客户端
 type EmailClient struct {
-	Host     string
-	Port     int
-	Username string
-	Password string
+	Host     string // smtp 地址
+	Port     int    // 用户名
+	Username string // 密码
+	Password string // 端口
 	Message  *EmailMessage
 }
 
-// NewEmailClient 返回一个邮件客户端
-// host smtp地址
-// username 用户名
-// password 密码
-// port 端口
 func NewEmailClient(host, username, password string, port int, message *EmailMessage) *EmailClient {
 	return &EmailClient{
 		Host:     host,
@@ -63,28 +49,28 @@ func NewEmailClient(host, username, password string, port int, message *EmailMes
 }
 
 // SendMessage 发送邮件
-func (c *EmailClient) SendMessage() (bool, error) {
+func (ec *EmailClient) SendMessage() (bool, error) {
 
-	e := gomail.NewPlainDialer(c.Host, c.Port, c.Username, c.Password)
-	if 587 == c.Port {
-		e.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+	d := gomail.NewPlainDialer(ec.Host, ec.Port, ec.Username, ec.Password)
+	if 587 == ec.Port {
+		d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
 	}
 	dm := gomail.NewMessage()
-	dm.SetHeader("From", c.Message.From)
-	dm.SetHeader("To", c.Message.To...)
+	dm.SetHeader("From", ec.Message.From)
+	dm.SetHeader("To", ec.Message.To...)
 
-	if len(c.Message.Cc) != 0 {
-		dm.SetHeader("Cc", c.Message.Cc...)
+	if len(ec.Message.Cc) != 0 {
+		dm.SetHeader("Cc", ec.Message.Cc...)
 	}
 
-	dm.SetHeader("Subject", c.Message.Subject)
-	dm.SetBody(c.Message.ContentType, c.Message.Content)
+	dm.SetHeader("Subject", ec.Message.Subject)
+	dm.SetBody(ec.Message.ContentType, ec.Message.Content)
 
-	if c.Message.Attach != "" {
-		dm.Attach(c.Message.Attach)
+	if ec.Message.Attach != "" {
+		dm.Attach(ec.Message.Attach)
 	}
 
-	if err := e.DialAndSend(dm); err != nil {
+	if err := d.DialAndSend(dm); err != nil {
 		return false, err
 	}
 	return true, nil
