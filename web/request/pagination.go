c0845fa9b595752e04293fd2d5b3d76517560b6f
charlesbases <1162776833@qq.com>
2019-05-20 16:14:28 +0800

    pagination

--- web/request/pagination.go
+++ web/request/pagination.go
@@ -5,18 +5,18 @@ import (
 	"strconv"
 )
 
-func Pagination(r *http.Request) (start, limit int) {
-	start, err := strconv.Atoi(r.FormValue("start"))
-	if err != nil || start < 1 {
-		start = 1
+func Pagination(r *http.Request) (int, int) {
+	offset, err := strconv.Atoi(r.FormValue("offset"))
+	if err != nil || offset < 1 {
+		offset = 1
 	}
-	limit, err = strconv.Atoi(r.FormValue("limit"))
+	limit, err := strconv.Atoi(r.FormValue("limit"))
 	if err != nil || limit < 0 || limit > 200 {
 		limit = 20
 	}
-	return (start - 1) * limit, limit
+	return (offset - 1) * limit, limit
 }
 
-func Page(limit int, count int) int {
+func PageTotal(limit int, count int) int {
 	return (count + limit - 1) / limit
 }
