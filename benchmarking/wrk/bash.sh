01056b9c8f9ac88d8e6b36006fb43bcfe92f6179
charlesbases <1162776833@qq.com>
2019-06-27 15:48:53 +0800

    benchmarking

--- benchmarking/wrk/bash.sh
+++ benchmarking/wrk/bash.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
-ip=www.baidu.com    # IP
-threads=10          # 线程数量
-duration=60s        # 压测时间
-connections=1000    # TCP连接数量
+ip=http://www.baidu.com         # IP
+threads=10                      # 线程数量
+duration=1m                     # 压测时间
+connections=1000                # TCP连接数量
 
-wrk -t $threads -d $duration -c $connections --latency https://www.baidu.com
+wrk -t $threads -d $duration -c $connections --latency $ip
