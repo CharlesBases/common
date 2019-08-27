a860ff4c22f52d13886d518f5062970338755aef
charlesbases <1162776833@qq.com>
2019-08-27 11:27:09 +0800

    proxysql

--- cluster/databases/proxysql/proxysql.sh
+++ cluster/databases/proxysql/proxysql.sh
@@ -1,47 +1,66 @@
 #!/bin/bash
 
-admin_port=6032
-mysql_port=6033
+# Docker
+name=proxysql
+admin_port=6032     # ProxySQL 管理端口
+mysql_port=6033     # ProxySQL 业务端口( 用于连接 MySQL )
 
-name=proxysql                                       # container name
-proxysql=/home/root/mysql/proxysql                  # proxysql dir
+# ProxySQL
+proxysql_admin=administrator                    # ProxySQL admin 凭证
+proxysql_admin_password=administrator
+proxysql_writer_server=10.10.10.10              # MySQL master 服务器
+proxysql_writer_server_port=3306
+proxysql_reader_server=10.10.10.20              # MySQL slave 服务器
+proxysql_reader_server_port=3306
+proxysql_master_writer=proxysql                 # MySQL master 业务账户(用于写数据，以及 slave 崩溃时读数据)
+proxysql_master_writer_password=proxysql
+proxysql_master_monitor=monitor                 # MySQL master 监控账户
+proxysql_master_monitor_password=monitor
+proxysql_slave_reader=slave                     # MySQL slave 业务账户
+proxysql_slave_reader_password=slave
 
-data=${proxysql}/data
-conf=${proxysql}/proxysql.cnf
+# MySQL
+mysql_version=8.0.17
 
-rm -rf ${proxysql}
+# config
+proxysql_dir=/home/root/mysql/proxysql
+
+data=${proxysql_dir}/data
+conf=${proxysql_dir}/proxysql.cnf
+
+rm -rf ${proxysql_dir}
 mkdir -p ${data}
 
 echo '
-datadir="/var/lib/proxysql"                         # 数据目录
+datadir="/var/lib/proxysql"
 
 admin_variables =
 {
-	admin_credentials = "administrator:administrator" # admin 凭证
-	mysql_ifaces = "0.0.0.0:6032"                     # admin 管理端口
+	admin_credentials = "'${proxysql_admin}':'${proxysql_admin_password}'"
+	mysql_ifaces = "0.0.0.0:6032"
 	refresh_interval = 2000
 	# debug = true
 }
 
 mysql_variables =
 {
-	threads = 4                                       # 转发端口的线程数量，建议与CPU核心数相等
+	threads = 4
 	max_connections = 2048
 	default_query_delay = 0
 	default_query_timeout = 36000000
 	have_compress = true
 	poll_timeout = 2000
-	interfaces = "0.0.0.0:6033;/tmp/proxysql.sock"    # mysql 代理端口
+	interfaces = "0.0.0.0:6033;/tmp/proxysql.sock"
 	default_schema = "information_schema"
 	stacksize = 1048576
-	server_version = "8.0.17"                         # mysql 版本
+	server_version = "'${mysql_version}'"
 	connect_timeout_server = 10000
 	ping_interval_server_msec = 10000
 	ping_timeout_server = 200
 	commands_stats = true
 	sessions_sort = true
-    monitor_username = "monitor"
-    monitor_password = "monitor"
+    monitor_username = "'${proxysql_master_monitor}'"
+    monitor_password = "'${proxysql_master_monitor_password}'"
     monitor_history = 60000
     monitor_connect_interval = 60000
     monitor_connect_timeout = 3000
@@ -55,26 +74,26 @@ mysql_variables =
 mysql_servers =
 (
     {
-		hostgroup = 10              # master group
-	    address = "10.10.10.10"
-	    port = 3306
+		hostgroup = 10
+	    address = "'${proxysql_writer_server}'"
+	    port = '${proxysql_writer_server_port}'
 		weight = 1
 	    status = "ONLINE"
-	    max_connections = 1000      # 最大连接
-    },
-    {
-		hostgroup = 20              # slave group
-		address = "10.10.10.20"
-		port = 3306
-		weight = 1
-		status = "ONLINE"
-	    max_connections = 1000      # 最大连接
-	    max_replication_lag = 10    # 最大延迟 (只适用于从节点)
+	    max_connections = 1000
     },
     {
 		hostgroup = 20
-	    address = "10.10.10.10"
-	    port = 3306
+		address = "'${proxysql_reader_server}'"
+		port = '${proxysql_reader_server_port}'
+		weight = 1
+		status = "ONLINE"
+	    max_connections = 1000
+	    max_replication_lag = 10
+    },
+    {
+		hostgroup = 20
+	    address = "'${proxysql_writer_server}'"
+	    port = '${proxysql_writer_server_port}'
 		weight = 10
 	    status = "ONLINE"
 	    max_connections = 1000
@@ -84,15 +103,15 @@ mysql_servers =
 mysql_users =
 (
     {
-        username = "proxysql"
-        password = "proxysql"
+        username = "'${proxysql_master_writer}'"
+        password = "'${proxysql_master_writer_password}'"
         default_hostgroup = 10
         max_connections = 1000
         active = 1
     },
     {
-        username = "monitor"
-        password = "monitor"
+        username = "'${proxysql_slave_reader}'"
+        password = "'${proxysql_slave_reader_password}'"
         default_hostgroup = 20
         max_connections = 1000
         active = 1
@@ -142,10 +161,10 @@ mysql_replication_hostgroups =
 #		}
 #	)
 
-' > ${proxysql}/proxysql.cnf
+' > ${proxysql_dir}/proxysql.cnf
 
 container_id=$(docker ps -a | grep ${name} | awk '{print $1}')
-if [ ${#container_id[@]} -gt 0 ]
+if [ ${#container_id} -ne 0 ]
 then
 	docker rm -f ${container_id}
 fi
@@ -159,5 +178,3 @@ docker run \
 	-d \
 	--name=${name} \
 	proxysql/proxysql
-
-# docker logs -f $(docker ps -l -q)
