da305e2cbc1e5d383345309794370b3b1a00851f
sun <sunzhiming@x-y-t.cn>
2021-03-04 17:27:13 +0800

    add rpc

---
+++ http/handler/rpc/rpc.go
@@ -0,0 +1,14 @@
+package rpc
+
+import "net/http"
+
+// rpc .
+type rpc struct{}
+
+// NewHandler .
+func NewHandler() *rpc {
+	return new(rpc)
+}
+
+func (rpc *rpc) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+}
